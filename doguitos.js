a91c7118c9a6b876458f072229bb5a4639dac90c
yuri costa <yuri342costa@gmail.com>
2021-05-19 15:49:02 -0300

    stretch

--- doguitos.js
+++ doguitos.js
@@ -52,34 +52,35 @@ dogs = [
     }
 
     function mudaTudo(){
-      
+
       var audio = new Audio('Ibageins/vitasforro.mp3');
       audio.addEventListener('canplaythrough', function() {
         audio.play();
       })
-        document.body.style.backgroundImage = "url('Ibageins/vitasg.gif')"
-        document.getElementsByClassName("site")[0].innerHTML= "SITE DO VITAS"
-        document.getElementById("dog1").getElementsByClassName("sammy")[0].innerHTML= "vitas"
-        document.getElementById("dog2").getElementsByClassName("dragao")[0].innerHTML= "vitas2"
-        document.getElementById("dog3").getElementsByClassName("milton")[0].innerHTML= "vitas3"
-        document.getElementById("dog4").getElementsByClassName("penny")[0].innerHTML= "vitas4"
-        document.getElementById("dog5").getElementsByClassName("nenem")[0].innerHTML= "vitas5"
-        document.getElementById("dog1").getElementsByClassName("imagem1")[0].src = "Ibageins/vitas2.jpg"
-        document.getElementById("dog2").getElementsByClassName("imagem2")[0].src = "Ibageins/xu.jpg"
-        document.getElementById("dog3").getElementsByClassName("imagem3")[0].src = "Ibageins/images.jpg"
-        document.getElementById("dog4").getElementsByClassName("imagem4")[0].src = "Ibageins/índice.jpg"
-        document.getElementById("dog5").getElementsByClassName("imagem5")[0].src = "Ibageins/images v.jpg"
-        document.getElementsByTagName("h2")[0].style.color = "blue"
-        document.getElementsByTagName("h2")[1].style.color = "purple"
-        document.getElementsByTagName("h2")[2].style.color = "yellow"
-        document.getElementsByTagName("h2")[3].style.color = "black"
-        document.getElementsByTagName("h2")[4].style.color = "brown"
-        document.getElementsByTagName("h2")[5].style.color = "white"
-        
-        
-    
+      document.body.style.backgroundImage = "url('Ibageins/vitasg.gif')"
+      document.getElementsByClassName("site")[0].innerHTML= "SITE DO VITAS"
+      document.getElementById("dog1").getElementsByClassName("sammy")[0].innerHTML= "vitas"
+      document.getElementById("dog2").getElementsByClassName("dragao")[0].innerHTML= "vitas2"
+      document.getElementById("dog3").getElementsByClassName("milton")[0].innerHTML= "vitas3"
+      document.getElementById("dog4").getElementsByClassName("penny")[0].innerHTML= "vitas4"
+      document.getElementById("dog5").getElementsByClassName("nenem")[0].innerHTML= "vitas5"
+      document.getElementById("dog1").getElementsByClassName("imagem1")[0].src = "Ibageins/vitas2.jpg"
+      document.getElementById("dog2").getElementsByClassName("imagem2")[0].src = "Ibageins/xu.jpg"
+      document.getElementById("dog3").getElementsByClassName("imagem3")[0].src = "Ibageins/images.jpg"
+      document.getElementById("dog4").getElementsByClassName("imagem4")[0].src = "Ibageins/índice.jpg"
+      document.getElementById("dog5").getElementsByClassName("imagem5")[0].src = "Ibageins/images v.jpg"
+      document.getElementsByTagName("h2")[0].style.color = "blue"
+      document.getElementsByTagName("h2")[1].style.color = "purple"
+      document.getElementsByTagName("h2")[2].style.color = "yellow"
+      document.getElementsByTagName("h2")[3].style.color = "black"
+      document.getElementsByTagName("h2")[4].style.color = "brown"
+      document.getElementsByTagName("h2")[5].style.color = "white"
+      
+      
+     document.alert(`aumenta o volume!!!!    ヽ(^。^)丿   `)
+      
     }
-     function soma() {
+    function soma() {
         var n1 = Number.parseFloat(window.prompt("digite um numero!"))
         var n2 = Number.parseFloat(window.prompt("digite outro numero para ser somado"))
         var soma = n1 + n2
